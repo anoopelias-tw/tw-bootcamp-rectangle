c549d8281c22207541731c69e953b2ec5418ea5e
Anoop Elias <Anoop.Elias@thoughtworks.com>
2023-11-14 09:01:21 +0530

    Calculate perimeter of a rectangle.

--- src/test/java/org/rectangle/RectangleTest.java
+++ src/test/java/org/rectangle/RectangleTest.java
@@ -17,4 +17,16 @@ public class RectangleTest {
         Rectangle rectangle = new Rectangle(1.5, 2.25);
         assertEquals(3.375, rectangle.area());
     }
+
+    @Test
+    public void testPerimeter_16_ForLength_3_Breadth_5() {
+        Rectangle rectangle = new Rectangle(3, 5);
+        assertEquals(16, rectangle.perimeter());
+    }
+
+    @Test
+    public void testPerimeter_7p5_ForLength_1p5_Breadth_2p25() {
+        Rectangle rectangle = new Rectangle(1.5, 2.25);
+        assertEquals(7.5, rectangle.perimeter());
+    }
 }
