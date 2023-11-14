324b01a4a498fd784706a3acc40153a2f36ee732
Anoop Elias <Anoop.Elias@thoughtworks.com>
2023-11-14 10:24:09 +0530

    Support creating square object using `Rectangle` class.

--- src/main/java/org/rectangle/Rectangle.java
+++ src/main/java/org/rectangle/Rectangle.java
@@ -5,6 +5,14 @@ public class Rectangle {
     private final double length;
     private final double breadth;
 
+    public static Rectangle square(double size) {
+        return new Rectangle(size, size);
+    }
+
+    public static Rectangle rectangle(double length, double breadth) {
+        return new Rectangle(length, breadth);
+    }
+
     public Rectangle(double length, double breadth) {
         this.length = length;
         this.breadth = breadth;
@@ -13,7 +21,6 @@ public class Rectangle {
     public double area() {
         return length * breadth;
     }
-
     public double perimeter() {
         return (length * 2) + (breadth * 2);
     }
--- src/test/java/org/rectangle/RectangleTest.java
+++ src/test/java/org/rectangle/RectangleTest.java
@@ -3,30 +3,38 @@ package org.rectangle;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.rectangle.Rectangle.rectangle;
+import static org.rectangle.Rectangle.square;
 
 public class RectangleTest {
 
     @Test
     public void testArea15ForLength3Breadth5() {
-        Rectangle rectangle = new Rectangle(3, 5);
+        Rectangle rectangle = rectangle(3, 5);
         assertEquals(15, rectangle.area());
     }
 
     @Test
     public void testArea_3p375_ForLength_1p5_Breadth2p25() {
-        Rectangle rectangle = new Rectangle(1.5, 2.25);
+        Rectangle rectangle = rectangle(1.5, 2.25);
         assertEquals(3.375, rectangle.area());
     }
 
     @Test
     public void testPerimeter_16_ForLength_3_Breadth_5() {
-        Rectangle rectangle = new Rectangle(3, 5);
+        Rectangle rectangle = rectangle(3, 5);
         assertEquals(16, rectangle.perimeter());
     }
 
     @Test
     public void testPerimeter_7p5_ForLength_1p5_Breadth_2p25() {
-        Rectangle rectangle = new Rectangle(1.5, 2.25);
+        Rectangle rectangle = rectangle(1.5, 2.25);
         assertEquals(7.5, rectangle.perimeter());
+    }
+
+    @Test
+    public void testArea_2p25_Square_size_1p5() {
+        Rectangle square = square(1.5);
+        assertEquals(2.25, square.area());
     }
 }
